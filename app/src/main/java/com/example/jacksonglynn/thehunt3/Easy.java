c6ff3d97b70b10502eae917eb237ec2295c0d288
Jackson Glynn <jacksonglynn@yahoo.com>
2017-02-21 14:46:29 -0500

    not finished at all check list view

--- app/src/main/java/com/example/jacksonglynn/thehunt3/Easy.java
+++ app/src/main/java/com/example/jacksonglynn/thehunt3/Easy.java
@@ -1,11 +1,17 @@
 package com.example.jacksonglynn.thehunt3;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
 
 /***************************************************************************************************
  *
@@ -15,8 +21,14 @@ import android.widget.Button;
  * @version (2/20/2017)
  **************************************************************************************************/
 public class Easy extends ScavengerHunt {
+    ArrayAdapter<String> m_adapter;
+    ArrayList<String> m_listItems = new ArrayList<String>();
+
     /*Creates the buttons on the main screen */
     Button add, finish, back;
+
+    EditText enter;
+    ListView list;
 
     /*Creates the Text shown in the xml*/
     private TextView clue, pic;
@@ -38,6 +50,10 @@ public class Easy extends ScavengerHunt {
         //creates the text pic and links it to the correct text box in the xml
         pic = (TextView) findViewById(R.id.pic);
 
+        enter = (EditText) findViewById(R.id.enter);
+
+        list = (ListView) findViewById(R.id.list);
+
         //creates the add button and links it to the correct button in the class
         add = (Button) findViewById(R.id.add);
 
@@ -46,6 +62,10 @@ public class Easy extends ScavengerHunt {
 
         //creates the finish button and links it to the correct button in the class
         finish = (Button) findViewById(R.id.finish);
+
+        m_adapter = new ArrayAdapter<String>(this, R.layout.easy_screen, m_listItems);
+        list.setAdapter(m_adapter);
+        final String input = enter.getText().toString();
 
         //Tells the text box clue what to put into it.
         clue.setText("Enter Your Clue: ");
@@ -65,12 +85,15 @@ public class Easy extends ScavengerHunt {
 
             /***************************************************************************************
              * tells the back button to go back to the easy class
+             *
              * @param v - the button when clicked
              **************************************************************************************/
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Easy.class);
-                startActivity(intent);
+                m_listItems.add(new String(input));
+                m_adapter.notifyDataSetChanged();
+                //Intent intent = new Intent(getApplicationContext(), Easy.class);
+               // startActivity(intent);
             }
         });
 
@@ -84,6 +107,7 @@ public class Easy extends ScavengerHunt {
 
             /***************************************************************************************
              * tells the back button to go back to the finishedEasy class
+             *
              * @param v - the button when clicked
              **************************************************************************************/
             @Override
@@ -103,6 +127,7 @@ public class Easy extends ScavengerHunt {
 
             /***************************************************************************************
              * tells the back button to go back to the local class
+             *
              * @param v - the button when clicked
              **************************************************************************************/
             @Override
@@ -111,5 +136,7 @@ public class Easy extends ScavengerHunt {
                 startActivity(intent);
             }
         });
-    }
+
+  }
 }
+
