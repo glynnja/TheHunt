4c48c18413abb97073b7a67c3a27661b6332b843
LenoXMasonJAR <mahonema5@gmail.com>
2017-02-21 14:05:26 -0500

    Changed

--- app/src/main/java/com/example/jacksonglynn/thehunt3/FinishedEasy.java
+++ app/src/main/java/com/example/jacksonglynn/thehunt3/FinishedEasy.java
@@ -1,7 +1,7 @@
 package com.example.jacksonglynn.thehunt3;
 
 /**
- * Created by Jackson Glynn on 2/19/2017.
+ * Created by Jackson Glynn and Mason Mahoney on 2/19/2017.
  */
 
 public class FinishedEasy {
