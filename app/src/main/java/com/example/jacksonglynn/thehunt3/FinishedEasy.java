a822761a22bf6559df285b5a8994f13fc8905747
Austin <austinvkemp@gmail.com>
2017-02-27 23:25:52 -0500

    small fixes

--- app/src/main/java/com/example/jacksonglynn/thehunt3/FinishedEasy.java
+++ app/src/main/java/com/example/jacksonglynn/thehunt3/FinishedEasy.java
@@ -1,54 +1,86 @@
 package com.example.jacksonglynn.thehunt3;
 
+/** AND AUSTIN
+ *
+ * Created by Jackson Glynn and Mason Mahoney2 on 2/19/2017.
+ */
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.SparseBooleanArray;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
-/***************************************************************************************************
- *
- * The following class generates the Easy page of the Scavenger Hunt game.
- *
- * @author Jackson Glynn, Mason Mahoney, Austin van Kempen
- * @version (2/20/2017)
- **************************************************************************************************/
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class FinishedEasy extends Easy {
-    /*Creates the ListView shown in the xml*/
+
     ListView hunt;
+    private TextView textView;
+    Button back, load;
 
-    /*Creates the button on the main screen */
-    Button back;
-
-
-    /***********************************************************************************************
-     *Creates the look of the easy screen and tell the texts what to put in it. It also
-     * tells the buttons what to do when clicked.
-     *@param savedInstanceState - creates the xml layout
-     **********************************************************************************************/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.easy_hunt);
 
-        //creates the listView and links it to the correct listView in the class
+
+
         hunt = (ListView) findViewById(R.id.hunt);
-
-        //creates the back button and links it to the correct button in the class
         back = (Button) findViewById(R.id.back);
+        load = (Button) findViewById(R.id.load);
+        textView = (TextView) findViewById(R.id.textView);
 
-        //creates the list view and sets up how it looks and acts
+
         final ArrayAdapter<String> adpt = new ArrayAdapter<String>(this,
+               // android.R.layout.simple_expandable_list_item_1, m_listItems);
          android.R.layout.simple_list_item_multiple_choice, m_listItems);
+
         hunt.setAdapter(adpt);
         hunt.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
+        SparseBooleanArray checked = hunt.getCheckedItemPositions();
+
+
 
         /*******************************************************************************************
-         * Creates the button listener for back. This tells the buttons what to do when it is
+         * Creates the button listener for add. This tells the buttons what to do when it is
          * clicked.
          ******************************************************************************************/
+        load.setOnClickListener(new View.OnClickListener() {
+
+
+            /***************************************************************************************
+             * tells the back button to go back to the easy class
+             *
+             * @param v - the button when clicked
+             **************************************************************************************/
+            @Override
+            public void onClick(View v) {
+
+                    Intent intent = getIntent();
+
+                        String cunt = intent.getStringExtra("list");
+
+                textView.setText(cunt);
+
+                textView.setMovementMethod(new ScrollingMovementMethod());
+
+
+
+
+            }
+        });
+
+
         back.setOnClickListener(new View.OnClickListener() {
 
 
