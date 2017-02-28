aceea21048fa4a827b61eda0ae11ef3b22229875
Jackson Glynn <jacksonglynn@yahoo.com>
2017-02-27 22:30:30 -0500

    java style guide almost complete

--- app/src/main/java/com/example/jacksonglynn/thehunt3/Easy.java
+++ app/src/main/java/com/example/jacksonglynn/thehunt3/Easy.java
@@ -1,7 +1,5 @@
 package com.example.jacksonglynn.thehunt3;
 
-import android.app.Activity;
-import android.icu.text.DisplayContext;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.ArrayAdapter;
@@ -11,11 +9,7 @@ import android.widget.TextView;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-
-import java.io.Serializable;
-import java.util.AbstractList;
 import java.util.ArrayList;
-import java.util.List;
 
 /***************************************************************************************************
  *
@@ -25,25 +19,21 @@ import java.util.List;
  * @version (2/20/2017)
  **************************************************************************************************/
 public class Easy extends ScavengerHunt {
-   // ArrayAdapter<String> m_adapter;
- //  ArrayList<List> m_listItems = new ArrayList<List>();
-
+    /*Creates the ArrayList shown in the xml and declares it as a static variable*/
    public static ArrayList<String> m_listItems = new ArrayList<String>();
-  //  String m_listItems[];
-    //    public ArrayList<String> getM_listItems(){
-//        return m_listItems;
-//    }
 
-  //  ArrayList<String> clone = new ArrayList<>();
     /*Creates the buttons on the main screen */
     Button add, finish, back;
 
+    /*Creates the EditText shown in the xml*/
     EditText enter;
+
+    /*Creates the ListView shown in the xml*/
     ListView list;
 
     /*Creates the Text shown in the xml*/
-    private TextView clue, pic, test;
-    public String maybe;
+    private TextView clue;
+
 
     /***********************************************************************************************
      *Creates the look of the easy screen and tell the texts what to put in it. It also
@@ -58,13 +48,10 @@ public class Easy extends ScavengerHunt {
         //creates the text clue and links it to the correct text box in the xml
         clue = (TextView) findViewById(R.id.clue);
 
-        //creates the text pic and links it to the correct text box in the xml
-        pic = (TextView) findViewById(R.id.pic);
-        test = (TextView) findViewById(R.id.test);
-
+        //creates the editText and links it to the correct editText in the class
         enter = (EditText) findViewById(R.id.enter);
 
-
+        //creates the listView and links it to the correct listView in the class
         list = (ListView) findViewById(R.id.list);
 
         //creates the add button and links it to the correct button in the class
@@ -76,24 +63,15 @@ public class Easy extends ScavengerHunt {
         //creates the finish button and links it to the correct button in the class
         finish = (Button) findViewById(R.id.finish);
 
-       // m_adapter = new ArrayAdapter<String>(this, R.layout.easy_screen, m_listItems);
-       // list.setAdapter(m_adapter);
-        //final String input = enter.getText().toString();
-
+        //creates the list view and sets up how it looks and acts
         final ArrayAdapter<String> adpt = new ArrayAdapter<String>(this,
-               // android.R.layout.simple_expandable_list_item_1, m_listItems);
                 android.R.layout.simple_list_item_multiple_choice, m_listItems);
         list.setAdapter(adpt);
         list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
-
         //Tells the text box clue what to put into it.
         clue.setText("Enter Your Clue: ");
         clue.setMovementMethod(new ScrollingMovementMethod());
-
-        //Tells the text box pic what to put into it.
-        pic.setText("Add A Picture: ");
-        pic.setMovementMethod(new ScrollingMovementMethod());
 
 
         /*******************************************************************************************
@@ -110,24 +88,15 @@ public class Easy extends ScavengerHunt {
              **************************************************************************************/
             @Override
             public void onClick(View v) {
-                //m_listItems.add(enter.gettoString());
                 String ag = enter.getText().toString().trim();
+
                 if(ag.length() != 0){
                     m_listItems.add(ag);
                     enter.setText("");
                 }
-                //m_listItems.add(enter.getText().toString().trim());
-                //m_listItems.add(enter.toString());
+
                 adpt.setNotifyOnChange(true);
                 list.setAdapter(adpt);
-                test.setText(m_listItems.toString());
-                test.setMovementMethod(new ScrollingMovementMethod());
-                //clone.add(enter.getText().toString());
-//                Intent intent = new Intent (m_listItems.add(enter.getText().toString()));
-              //  m_listItems.add(new String(input));
-              //  m_adapter.notifyDataSetChanged();
-               // Intent intent = new Intent(getApplicationContext(), Camera.class);
-               // startActivity(intent);
             }
         });
 
@@ -136,13 +105,6 @@ public class Easy extends ScavengerHunt {
          * Creates the button listener for finish. This tells the buttons what to do when it is
          * clicked.
          ******************************************************************************************/
-//        list.setOnClickListener(new View.OnClickListener(){
-//        @Override
-//            public void onClick(View v){
-//
-//        }
-//        });
-
         finish.setOnClickListener(new View.OnClickListener() {
 
 
@@ -153,17 +115,8 @@ public class Easy extends ScavengerHunt {
              **************************************************************************************/
             @Override
             public void onClick(View v) {
-              //  m_listItems.addAll(clone);
-                //ArrayList<String> clone = new ArrayList<>();
-                //clone.addAll(m_listItems);
-               // savedInstanceState.getStringArrayList(m_listItems.toString());
-               // list.setText(getIntent().getStringExtra("m_listItems"));
                 Intent intent = new Intent(getApplicationContext(), FinishedEasy.class);
                 intent.putExtra("list", m_listItems);
-//                for(int i =0; i < m_listItems.size();i++) {
-//                    intent.putExtra(m_listItems.set(i, m_listItems.toString()), m_listItems);
-//                }
-               // Intent intent = new Intent(getApplicationContext(), FinishedEasy.class);
                 startActivity(intent);
                 finish();
             }
--- app/src/main/java/com/example/jacksonglynn/thehunt3/FinishedEasy.java
+++ app/src/main/java/com/example/jacksonglynn/thehunt3/FinishedEasy.java
@@ -1,149 +1,50 @@
 package com.example.jacksonglynn.thehunt3;
 
-/** AND AUSTIN
- *
- * Created by Jackson Glynn and Mason Mahoney2 on 2/19/2017.
- */
-import android.app.Activity;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+/***************************************************************************************************
+ *
+ * The following class generates the Easy page of the Scavenger Hunt game.
+ *
+ * @author Jackson Glynn, Mason Mahoney, Austin van Kempen
+ * @version (2/20/2017)
+ **************************************************************************************************/
 public class FinishedEasy extends Easy {
-    //private ArrayList m_listItems = Easy.m_listItems;
-   // ArrayList<String> cloned = new ArrayList<String>();
-   // String[] array = getIntent().getExtras().getStringArray("list");
-   // ArrayList<String> cloned = new ArrayList<String>();
+    /*Creates the ListView shown in the xml*/
     ListView hunt;
-    private TextView textView;
-    Button back, load;
 
+    /*Creates the button on the main screen */
+    Button back;
+
+
+    /***********************************************************************************************
+     *Creates the look of the easy screen and tell the texts what to put in it. It also
+     * tells the buttons what to do when clicked.
+     *@param savedInstanceState - creates the xml layout
+     **********************************************************************************************/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.easy_hunt);
 
-       // Intent intent = new Intent(getApplicationContext(), FinishedEasy.class);
-        //startActivity(intent);
-//        Bundle b = getIntent().getExtras();
-//
-//        if(b!=null) {
-//            ArrayList<String> clone = (ArrayList<String>) b.getStringArrayList(m_listItems.toString());
-//            textView.setText(clone.toString());
-//            textView.setMovementMethod(new ScrollingMovementMethod());
-//        }
-           // System.out.println(arr);
-
         hunt = (ListView) findViewById(R.id.hunt);
         back = (Button) findViewById(R.id.back);
-        load = (Button) findViewById(R.id.load);
-        textView = (TextView) findViewById(R.id.textView);
-
-       // m_listItems.addAll(m_listItems);
-
-//        textView.setText(m_listItems.toString());
-//        textView.setMovementMethod(new ScrollingMovementMethod());
-//        public static List<Easy> cloneList(List<Easy>, m_listItems){
-//            List<Easy> clone = new ArrayList<Easy>(m_listItems.size());
-//            for (Easy item : m_listItems) clone.add(item.clone());
-//            return clone;
-//        }
-       // Easy = onCreate.getList();
-        //Easy hunt = Collections.synchronizedList(new ArrayList(m_listItems));
-       //setContentView();
-        //m_listItems = getIntent().getStringArrayListExtra;
-       // clone.add(enter.getText().toString());
-      //  adpt.setNotifyOnChange(true);
-
-//        hunt.equals(list.toString());
-//        m_listItems.add(enter.getText().toString());
-       // clone.add(m_listItems.toString());
-//        clone.toString();
-//        m_listItems.add(list.toString());
-//        list.getCheckedItemCount();
-
-       //  clone.addAll(m_listItems);
-
-        // m_listItems.add(enter.getText().toString());
-       // clone.addAll(m_listItems);
 
         final ArrayAdapter<String> adpt = new ArrayAdapter<String>(this,
-               // android.R.layout.simple_expandable_list_item_1, m_listItems);
          android.R.layout.simple_list_item_multiple_choice, m_listItems);
-       // m_listItems.add(Easy.class.getName(list).toString());
-      //  m_listItems.add(list.toString());
-      //  adpt.setNotifyOnChange(true);
         hunt.setAdapter(adpt);
         hunt.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 
+
         /*******************************************************************************************
-         * Creates the button listener for add. This tells the buttons what to do when it is
+         * Creates the button listener for back. This tells the buttons what to do when it is
          * clicked.
          ******************************************************************************************/
-        load.setOnClickListener(new View.OnClickListener() {
-
-
-            /***************************************************************************************
-             * tells the back button to go back to the easy class
-             *
-             * @param v - the button when clicked
-             **************************************************************************************/
-            @Override
-            public void onClick(View v) {
-                //for(int i=0; i<m_listItems.indexOf(i); i++) {
-                    //clone.addAll(m_listItems);
-                    //hunt.addTouchables(list.toString());
-                    Intent intent = getIntent();
-                   // if(null != intent){
-                        String cunt = intent.getStringExtra("list");
-                   // }
-                   // hunt.equals(cunt);
-                   // textView.setText(m_listItems.toString());
-                textView.setText(cunt);
-
-                textView.setMovementMethod(new ScrollingMovementMethod());
-
-
-//                Bundle b = getIntent().getExtras();
-//
-//                if(b!=null) {
-//                    ArrayList<String> clone = (ArrayList<String>) b.getStringArrayList(m_listItems.toString());
-//                    textView.setText(clone.toString());
-//                    textView.setMovementMethod(new ScrollingMovementMethod());
-//                }
-               // }
-                //hunt.equals(list);
-              //  m_listItems.add();
-                ;
-               // m_listItems.add(enter.getText().toString());
-               // adpt.setNotifyOnChange(true);
-               // hunt.setAdapter(adpt);
-                //clone.addAll(m_listItems);
-//                Intent intent = new Intent (m_listItems.add(enter.getText().toString()));
-                //  m_listItems.add(new String(input));
-                //  m_adapter.notifyDataSetChanged();
-                // Intent intent = new Intent(getApplicationContext(), Camera.class);
-                // startActivity(intent);
-            }
-        });
-//        hunt.equals(list.toString());
-//        m_listItems.add(enter.getText().toString());
-       // public void onStart()
-//        m_listItems.add(list.toString());
-       // adpt.setNotifyOnChange(true);
-       // hunt.setAdapter(adpt);
-
         back.setOnClickListener(new View.OnClickListener() {
 
 
