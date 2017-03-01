5394b114dbdac1ec12776bd36dd5ac8061765fdc
Jackson Glynn <jacksonglynn@yahoo.com>
2017-03-01 18:46:53 -0500

    almost done

--- app/src/main/java/com/example/jacksonglynn/thehunt3/Finished.java
+++ app/src/main/java/com/example/jacksonglynn/thehunt3/Finished.java
@@ -1,6 +1,7 @@
 package com.example.jacksonglynn.thehunt3;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.ArrayAdapter;
 import android.widget.CompoundButton;
 import android.widget.ListAdapter;
@@ -8,47 +9,42 @@ import android.widget.ListView;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /***************************************************************************************************
  *
- * The following class generates the FinishedEasy page of the Scavenger Hunt game.
+ * The following class generates the Finished page of the Scavenger Hunt game.
  *
  * @author Jackson Glynn, Mason Mahoney, Austin van Kempen
  * @version (2/20/2017)
  **************************************************************************************************/
-public class FinishedEasy extends Easy {
-    /*Creates the ListView shown in the xml*/
-    ListView hunt;
-
+public class Finished extends FinishedEasy {
     /*Creates the button on the main screen */
-    Button back, done;
+    Button done;
 
+    /*Creates the Text shown in win*/
+    private TextView win;
 
     /***********************************************************************************************
-     *Creates the look of the FinishedEasy screen and tell the texts what to put in it. It also
+     *Creates the look of the Finished screen and tell the texts what to put in it. It also
      * tells the buttons what to do when clicked.
      *@param savedInstanceState - creates the xml layout
      **********************************************************************************************/
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.easy_hunt);
-
-
-        //creates the listView and links it to the correct listView in the class
-        hunt = (ListView) findViewById(R.id.hunt);
-
-        //creates the back button and links it to the correct button in the class
-        back = (Button) findViewById(R.id.back);
+        setContentView(R.layout.done_screen);
 
         //creates the back button and links it to the correct button in the class
         done = (Button) findViewById(R.id.done);
 
-        //creates the list view and sets up how it looks and acts
-        final ArrayAdapter<String> adpt = new ArrayAdapter<String>(this,
-         android.R.layout.simple_list_item_multiple_choice, m_listItems);
-        hunt.setAdapter(adpt);
-        hunt.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        //creates the text and links it to the correct text box in the xml
+        win = (TextView) findViewById(R.id.win);
+
+        //Tells the text box sorry what to put into it.
+        win.setText("YOU WIN!!!");
+        win.setMovementMethod(new ScrollingMovementMethod());
+
 
         /*******************************************************************************************
          * Creates the button listener for done. This tells the buttons what to do when it is
@@ -64,29 +60,8 @@ public class FinishedEasy extends Easy {
              **************************************************************************************/
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Finished.class);
-                startActivity(intent);
-            }
-        });
-
-
-
-        /*******************************************************************************************
-         * Creates the button listener for back. This tells the buttons what to do when it is
-         * clicked.
-         ******************************************************************************************/
-        back.setOnClickListener(new View.OnClickListener() {
-
-
-            /***************************************************************************************
-             * tells the back button to go back to the Easy class
-             *
-             * @param v - the button when clicked
-             **************************************************************************************/
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Easy.class);
-                startActivity(intent);
+                finish();
+                System.exit(0);
             }
         });
     }
