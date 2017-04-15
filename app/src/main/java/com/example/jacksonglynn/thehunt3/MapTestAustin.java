c52075bb6c5aa752499a1d79daffd785b5f47a4a
Austin <austinvkemp@gmail.com>
2017-04-15 17:40:09 -0400

    Try working with what I added. It might help. There are some errors, but I think it might be my computer

---
+++ app/src/main/java/com/example/jacksonglynn/thehunt3/MapTestAustin.java
@@ -0,0 +1,52 @@
+package com.example.jacksonglynn.thehunt3;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapTestAustin extends FragmentActivity {
+
+    private GoogleMap mMap; //Might be null if API isnt ready
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)  {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_test_austin);
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+
+    private void setUpMapIfNeeded() {
+        //Do a null check to confirm that we have set the map up
+        if (mMap == null) {
+            //Try to obtain map from the supportmapfragment
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            //Check if it worked
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+
+    private void setUpMap() {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0).title("Marker"));
+    }
+
+
+
+
+}
+
